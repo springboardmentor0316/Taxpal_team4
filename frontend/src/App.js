ac3b291d7e0736d1371557f2accd3c725250704c
darvesh aher <darvesh.aher25012005@gmail.com>
2025-09-18 14:25:08 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import OTP from "./pages/OTP";
+import ResetPassword from "./pages/ResetPassword";
+import AddIncome from "./pages/AddIncome";
+import AddExpense from "./pages/AddExpense";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import Budgets from "./pages/Budgets";
+import "./styles/global.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />{" "}
+        <Route path="/signup" element={<Signup />} />{" "}
+        <Route path="/forgot" element={<ForgotPassword />} />{" "}
+        <Route path="/otp" element={<OTP />} />{" "}
+        <Route path="/reset" element={<ResetPassword />} />{" "}
+        <Route path="/income/new" element={<AddIncome />} />{" "}
+        <Route path="/expense/new" element={<AddExpense />} />{" "}
+        <Route path="/home" element={<Home />} />{" "}
+        <Route path="/dashboard" element={<Dashboard />} />{" "}
+        <Route path="/budgets" element={<Budgets />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>{" "}
+      {/* ✅ Toasts appear at the top-right corner */}{" "}
+      <ToastContainer position="top-right" autoClose={3000} />{" "}
+    </BrowserRouter>
+  );
+}
